c25ab0a12d58759c058c09488db00b02aaccb86e
GeovaniPassos <geovanipassos95@outlook.com>
2026-07-20 16:57:30 -0300

    Ajustando o filtro do header

--- src/app/shared/header/header.ts
+++ src/app/shared/header/header.ts
@@ -1,14 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, input, signal } from '@angular/core';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
 import { RouterLink } from "@angular/router";
+import { MatFormField, MatLabel } from "@angular/material/input";
 
 @Component({
   selector: 'app-header',
-  imports: [RouterLink, MatToolbarModule, MatIconModule, MatButtonModule],
+  imports: [RouterLink, 
+    MatToolbarModule, 
+    MatButtonModule, 
+    MatFormField, 
+    MatLabel, 
+    MatIconModule],
+    
   templateUrl: './header.html',
   styleUrl: './header.scss',
 })
-export class Header {}
+export class Header {
+  protected readonly value = signal('');
+
+}
   
