ea42c0f6dfb84340a94fbbfc3c690d7b4308d2cb
Geovani Passos <geovanipassos95@outlook.com>
2026-08-07 22:46:19 -0300

    implementando o grid de exibicao dos componentes para montar as pizzas

--- src/app/shared/pizza-modal/pizza-modal.ts
+++ src/app/shared/pizza-modal/pizza-modal.ts
@@ -1,10 +1,17 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { A11yModule } from "@angular/cdk/a11y";
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatIcon } from "@angular/material/icon";
+import { MatFormField } from "@angular/material/input";
+import { ProductsCard } from "../../pages/produtcs/products-card/products-card";
+import { Products } from '../../models/products';
+import { ProductService } from '../../services/product-service/product-service';
 
 @Component({
   selector: 'app-pizza-modal',
-  imports: [MatExpansionModule, A11yModule],
+  imports: [MatExpansionModule, A11yModule, MatCheckboxModule, MatRadioModule, MatIcon, ProductsCard],
   templateUrl: './pizza-modal.html',
   styleUrl: './pizza-modal.scss',
 })
@@ -22,4 +29,7 @@ export class PizzaModal {
   prevStep() {
     this.step.update(i => i - 1);
   }
+
+  protected productService = inject(ProductService);
+
 }
